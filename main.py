2719b67483d0ab9f1e0182d385cd566d0889220f
Talina Bay <talinabayeleva@gmail.com>
2025-08-11 15:03:17 +0200

    Fix string formatting bug in onboarding page creation log

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from config import ONBOARDING_TASKS
 
 onboardees = csv_helpers.get_onboardees_from_csv("to_be_onboarded.csv")
 for onboardee in onboardees:
-    print(f"Creating onboarding page for {onboardee["onboardee"]} with role {onboardee["role"]}")
+    print(f'Creating onboarding page for {onboardee["onboardee"]} with role {onboardee["role"]}')
     onboardee_id = notion_helpers.create_onboarding_page(onboardee["onboardee"])
     for task in ONBOARDING_TASKS[onboardee["role"]]:
         notion_helpers.create_task(task, onboardee_id)
